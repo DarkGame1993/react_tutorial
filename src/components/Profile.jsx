ce16f82fe02a214e815e49e08c2294028324a3a7
v.skorokhod@knubisoft.com <v.skorokhod@knubisoft.com>
2019-03-29 19:07:44 +0200

    add structure

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'></img>
+            </div>
+            <div>
+                ava
+                <img src='https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/12/11/naturo-monkey-selfie.jpg?w968h681'></img>
+            </div>
+            <div>
+                My post
+            <div>
+                New post
+            </div>
+            </div>
+            <div>post 1</div>
+            <div>post 2</div>
+      </div>
+    )
+    
+}
+
+export default Profile;
